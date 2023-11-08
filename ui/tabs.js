fe4cc7bd5893a8267166e628a731611e70253b9c
RupalSingla <rupalsingla15@gmail.com>
2023-11-08 13:07:13 +0530

    IGM UI Interface

---
+++ ui/tabs.js
@@ -0,0 +1,19 @@
+
+// tabs.js
+
+function onFirstLoad(build_spec){
+      let data = build_spec
+    //   initSchema(data["x-enum"])
+    //   initTag(data["x-tags"])
+    //   loadExample(data["x-examples"])
+      //addExample("on-demand")
+    //   loadFlows(data["x-flows"])
+      loadAttributes(data["x-attributes"])
+    //   loadErrors(data["x-errorcodes"])
+    //   loadTlc(data["x-tlc"])
+    //   loadtestcase(data["x-testcase"])
+}
+
+window.onload = function(){
+      onFirstLoad(build_spec)
+} 
