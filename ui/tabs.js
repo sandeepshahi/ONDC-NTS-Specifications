1ab889ac75a6012dcba4108901fe82a5195f69c8
RupalSingla <rupalsingla15@gmail.com>
2024-08-20 12:10:24 +0530

    enums

--- ui/tabs.js
+++ ui/tabs.js
@@ -2,7 +2,7 @@
 
 function onFirstLoad(build_spec) {
   let data = build_spec;
-  // initSchema(data["x-enum"])
+  initSchema(data["x-enum"])
   // loadAttributes(data["x-attributes"])
   loadFlows(data["x-flows"]);
   //   initTag(data["x-tags"])
